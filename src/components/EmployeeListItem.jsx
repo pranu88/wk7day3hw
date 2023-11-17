eab964674446b0937919658a66b7141f1c6588e5
pranu88 <pranusha.pothugunta@gmail.com>
2023-11-16 21:36:03 -0500

    picture

--- src/components/EmployeeListItem.jsx
+++ src/components/EmployeeListItem.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function EmployeeListItem() {
     const [data,setData] = useState(EmployeeInfo)
   return (
-    <div > {data.map((info)=>{return<div style={{border:"solid black"}}><h5>{info.name}</h5> <p>{info.job}</p> </div> })}</div>
+    <div style={{height:'20em'}}> {data.map((info)=>{return<div style={{border:"solid black", lineHeight:"1px",height:"6rem"}}><img src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png" style={{width:"5%"}}/><h5>{info.name}</h5> <p>{info.job}</p> </div> })}</div>
   )
 }
 
