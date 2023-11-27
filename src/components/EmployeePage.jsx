5b254c3dd5c2a0a94f554c315ffd3dc3ec8f8956
pranu88 <pranusha.pothugunta@gmail.com>
2023-11-27 13:59:24 -0500

    employee page edited

--- src/components/EmployeePage.jsx
+++ src/components/EmployeePage.jsx
@@ -3,7 +3,14 @@ import Header from './Header'
 
 function EmployeePage() {
   return (
-    <div style={{border:'solid brown', width: '50%', height: '42em',margin:'2px'}} ><Header/></div>
+    <div style={{border:'solid brown', width: '50%', height: '42em',margin:'2px'}} >
+      <Header/>
+      <img src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png" style={{width:"25%",paddingLeft:'35%'}}/>
+      <div style={{border:'solid black', width:'100%',height:'4em',marginTop:'10px'}}>Call Office</div>
+      <div style={{border:'solid black', width:'100%',height:'4em'}}>Call Mobile</div>
+      <div style={{border:'solid black', width:'100%',height:'4em'}}>SMS</div>
+      <div style={{border:'solid black', width:'100%',height:'4em'}}>Email</div>
+     </div>
   )
 }
 
--- src/models/EmployeeInfo.jsx
+++ src/models/EmployeeInfo.jsx
@@ -2,32 +2,56 @@ const EmployeeInfo = [
     {
      name :'James King',
      job:'President and CEO',
+     mobilenumber: '000-000-000', 
+     officenumber:'111-111-1111',
+     sms:'222-222-2222',
+     email:"abc@gmail.com",
     //  profile:<img src={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>
     },
     {
        name: 'Julie Taylor',
        job:'VP of Marketing',
+       mobilenumber: '000-000-000', 
+       officenumber:'111-111-1111',
+       sms:'222-222-2222',
+       email:"abc@gmail.com",
     //    profile:<img src={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>
     },
        
     {
      name:'Eugene Lee',
      job:'CFO',
+     mobilenumber: '000-000-000', 
+     officenumber:'111-111-1111',
+     sms:'222-222-2222',
+     email:"abc@gmail.com",
     //  profile:<img src={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>
    },
     {
      name:'John Williams',
      job:'VP of Engineering',
+     mobilenumber: '000-000-000', 
+     officenumber:'111-111-1111',
+     sms:'222-222-2222',
+     email:"abc@gmail.com",
     //  profile:<img src={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>
     },
     {
     name:'Ray Moore',
     job:'VP of States',
+    mobilenumber: '000-000-000', 
+    officenumber:'111-111-1111',
+    sms:'222-222-2222',
+    email:"abc@gmail.com",
     // profile: <img src={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>,
     },
     {
     name:'Paul Jones',
     job:'QA Manager',
+    mobilenumber: '000-000-000', 
+    officenumber:'111-111-1111',
+    sms:'222-222-2222',
+    email:"abc@gmail.com",
     // profile:<img src ={'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png'}/>,
     }
 ]
